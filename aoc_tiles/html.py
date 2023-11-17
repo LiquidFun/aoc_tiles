c37db90fe06f005ece1f94dc4f0e138e0fa9a93e
LiquidFun <brutenis@gmail.com>
2023-11-17 20:50:26 +0100

    Add config

---
+++ aoc_tiles/html.py
@@ -0,0 +1,39 @@
+class HTMLTag:
+    """A convenience class for creating HTML tags, can be used in a with statement, which will close the tag."""
+    def __init__(self, parent: "HTML", tag: str, closing: bool = True, **kwargs):
+        self.parent = parent
+        self.tag = tag
+        self.closing = closing
+        self.kwargs = kwargs
+        attributes = "".join(f' {k}="{v}"' for k, v in self.kwargs.items())
+        self.parent.push(f"<{self.tag}{attributes}>", depth=self.closing)
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        if self.closing:
+            self.parent.push(f"</{self.tag}>", depth=-self.closing)
+
+
+class HTML:
+    """A convenience class to create HTML code, in order be able to close the tags and add the necessary syntax.
+
+    Originally, this was used a lot more, however at this point it is only needed for a single with statement.
+    """
+    tags: list[str] = []
+    depth = 0
+
+    def push(self, tag: str, depth=0):
+        if depth < 0:
+            self.depth += depth
+        self.tags.append("  " * self.depth + tag)
+        if depth > 0:
+            self.depth += depth
+
+    def tag(self, tag: str, closing: bool = True, **kwargs):
+        return HTMLTag(self, tag, closing, **kwargs)
+
+    def __str__(self):
+        return "\n".join(self.tags)
+
--- aoc_tiles/tiles.py
+++ aoc_tiles/tiles.py
@@ -24,7 +24,7 @@ from functools import cache
 from pathlib import Path
 import re
 import json
-from typing import Literal
+from typing import Literal, Union, List, Dict, Tuple
 
 import requests
 from PIL import Image, ImageColor
@@ -32,41 +32,10 @@ import yaml
 from PIL.ImageDraw import ImageDraw
 from PIL import ImageFont
 
-# This results in the parent directory of the script directory, the year directories should be here
-AOC_DIR = Path(__file__).absolute().parent.parent
-
-# The directory where the image files for the tiles are stored. This should be committed to git.
-# Year directories are created in this directory, then each day is saved as 01.png, 02.png, etc.
-IMAGE_DIR = AOC_DIR / "Media"
-
-# Path to the README file where the tiles should be added
-README_PATH = AOC_DIR / "README.md"
-
-# Path to the cookie session file
-SESSION_COOKIE_PATH = AOC_DIR / "session.cookie"
-
-# Whether the graphic should be created for days that have not been completed yet. Note that missing days between
-# completed days will still be created.
-CREATE_ALL_DAYS = False
-
-# Instead of showing the time and rank you achieved this just shows whether
-# it was completed with a checkmark
-SHOW_CHECKMARK_INSTEAD_OF_TIME_RANK = False
-
-# The year and day pattern to detect directories. For example, if your day folders are
-# called "day1" to "day25" then set the pattern to r"day\d{1,2}". The script extracts
-# a number from the folder and tries to guess its day that way.
-YEAR_PATTERN = r"\d{4}"
-DAY_PATTERN = r"\d{2}"
-
-
-# On how to improve legibility of the text when the background is white, outline will add a dark outline around
-# the text, "text" will make the text itself dark, none will not change the text color (leaves it white)
-CONTRAST_IMPROVEMENT_TYPE: Literal["none", "outline", "dark"] = "outline"
-
+from aoc_tiles.html import HTML
 
 # You can change this code entirely, or just change patterns above. You get more control if you change the code.
-def get_solution_paths_dict_for_years() -> dict[int, dict[int, list[str]]]:
+def get_solution_paths_dict_for_years(aoc_dir: Union[str, Path]) -> Dict[int, Dict[int, List[str]]]:
     """Returns a dictionary which maps years to days to a list of solution paths,
 
     E.g.: {2022: {1: [Path("2022/01/01.py"), Path("2022/01/01.kt")], ...}}
@@ -79,7 +48,7 @@ def get_solution_paths_dict_for_years() -> dict[int, dict[int, list[str]]]:
     (Untested however)
 
     """
-    solution_paths_dict: dict[int, dict[int, list[str]]] = {}
+    solution_paths_dict: Dict[int, Dict[int, List[str]]] = {}
 
     # If you use a new repo for years you might just remove this if, and assign the year manually
     for year_dir in sorted(get_paths_matching_regex(AOC_DIR, YEAR_PATTERN), reverse=True):
@@ -111,33 +80,11 @@ def get_solution_paths_dict_for_years() -> dict[int, dict[int, list[str]]]:
 # === The following likely do not need to be changed ===
 # ======================================================
 
-# Color if a part is not completed
-NOT_COMPLETED_COLOR = ImageColor.getrgb("#333333")
-OUTLINE_COLOR = ImageColor.getrgb("#6C6A6A")
-# Add outline if too bright ( = too similar to TEXT_WHITE)
-CONTRAST_IMPROVEMENT_THRESHOLD = 30  # Range from 0 to 255
-TEXT_COLOR = ImageColor.getrgb("#FFFFFF")
-
-# Width of each tile in the README.md.
-# 161px is a rather specific number, with it exactly 5 tiles fit into a row. It is possible to go
-# to 162px, however then 1080p displays show 4 tiles in a row, and phone displays show 1 tile
-# instead of 2 in a row. Therefore, 161px is used here.
-TILE_WIDTH_PX = "161px"
-
-# This results in the parent folder of the script file
-AOC_TILES_SCRIPT_DIR = Path(__file__).absolute().parent
-
-# Cache path is a subfolder of the AOC folder, it includes the personal leaderboards for each year
-CACHE_DIR = AOC_TILES_SCRIPT_DIR / ".aoc_tiles_cache"
-
-# Overrides day 24 part 2 and day 25 both parts to be unsolved
-DEBUG = False
-
 # URL for the personal leaderboard (same for everyone)
 PERSONAL_LEADERBOARD_URL = "https://adventofcode.com/{year}/leaderboard/self"
 
 # Location of yaml file where file extensions are mapped to colors
-GITHUB_LANGUAGES_PATH = AOC_DIR / "github_languages.yml"
+GITHUB_LANGUAGES_PATH = Path(__file__).parent / "github_languages.yml"
 
 
 @cache
@@ -164,14 +111,14 @@ def get_extension_to_colors():
     return extension_to_color
 
 
-extension_to_color: dict[str, str] = get_extension_to_colors()
+extension_to_color: Dict[str, str] = get_extension_to_colors()
 
 
 def get_paths_matching_regex(path: Path, pattern: str):
     return sorted([p for p in path.iterdir() if re.fullmatch(pattern, p.name)])
 
 
-def find_recursive_solution_files(directory: Path) -> list[Path]:
+def find_recursive_solution_files(directory: Path) -> List[Path]:
     solution_paths = []
     for path in directory.rglob("*"):
         if path.is_file() and path.suffix in extension_to_color:
@@ -179,7 +126,7 @@ def find_recursive_solution_files(directory: Path) -> list[Path]:
     return solution_paths
 
 
-def parse_leaderboard(leaderboard_path: Path) -> dict[int, DayScores]:
+def parse_leaderboard(leaderboard_path: Path) -> Dict[int, DayScores]:
     no_stars = "You haven't collected any stars... yet."
     start = '<span class="leaderboard-daydesc-both"> *Time *Rank *Score</span>\n'
     end = "</pre>"
@@ -200,7 +147,7 @@ def parse_leaderboard(leaderboard_path: Path) -> dict[int, DayScores]:
         return leaderboard
 
 
-def request_leaderboard(year: int) -> dict[int, DayScores]:
+def request_leaderboard(year: int) -> Dict[int, DayScores]:
     leaderboard_path = CACHE_DIR / f"leaderboard{year}.html"
     if leaderboard_path.exists():
         leaderboard = parse_leaderboard(leaderboard_path)
@@ -226,42 +173,8 @@ def request_leaderboard(year: int) -> dict[int, DayScores]:
     return parse_leaderboard(leaderboard_path)
 
 
-class HTMLTag:
-    def __init__(self, parent: "HTML", tag: str, closing: bool = True, **kwargs):
-        self.parent = parent
-        self.tag = tag
-        self.closing = closing
-        self.kwargs = kwargs
-        attributes = "".join(f' {k}="{v}"' for k, v in self.kwargs.items())
-        self.parent.push(f"<{self.tag}{attributes}>", depth=self.closing)
 
-    def __enter__(self):
-        pass
-
-    def __exit__(self, *args):
-        if self.closing:
-            self.parent.push(f"</{self.tag}>", depth=-self.closing)
-
-
-class HTML:
-    tags: list[str] = []
-    depth = 0
-
-    def push(self, tag: str, depth=0):
-        if depth < 0:
-            self.depth += depth
-        self.tags.append("  " * self.depth + tag)
-        if depth > 0:
-            self.depth += depth
-
-    def tag(self, tag: str, closing: bool = True, **kwargs):
-        return HTMLTag(self, tag, closing, **kwargs)
-
-    def __str__(self):
-        return "\n".join(self.tags)
-
-
-def darker_color(c: tuple[int, int, int, int]) -> tuple[int, int, int, int]:
+def darker_color(c: Tuple[int, int, int, int]) -> Tuple[int, int, int, int]:
     return c[0] - 10, c[1] - 10, c[2] - 10, 255
 
 
@@ -295,7 +208,13 @@ def get_alternating_background(languages, both_parts_completed=True, *, stripe_w
 
 
 def format_time(time: str) -> str:
-    """Formats time as mm:ss if the time is below 1 hour, otherwise it returns >1h to a max of >24h"""
+    """Formats time as mm:ss if the time is below 1 hour, otherwise it returns >1h to a max of >24h
+
+    >>> format_time("00:58:32")
+    '58:32'
+    >>> format_time(">1h")
+    '  >1h'
+    """
     time = time.replace("&gt;", ">")
     if ">" in time:
         formatted = time
@@ -305,17 +224,17 @@ def format_time(time: str) -> str:
     return f"{formatted:>5}"
 
 
-def draw_star(drawer: ImageDraw, at: tuple[int, int], size=9, color="#ffff0022", num_points=5):
+def draw_star(drawer: ImageDraw, at: Tuple[int, int], size=9, color="#ffff0022", num_points=5):
     """Draws a star at the given position"""
     diff = math.pi * 2 / num_points / 2
-    points: list[tuple[float, float]] = []
+    points: List[Tuple[float, float]] = []
     for angle in [diff * i - math.pi / 2 for i in range(num_points * 2)]:
         factor = size if len(points) % 2 == 0 else size * 0.4
         points.append((at[0] + math.cos(angle) * factor, at[1] + math.sin(angle) * factor))
     drawer.polygon(points, fill=color)
 
 
-def generate_day_tile_image(day: str, year: str, languages: list[str], day_scores: DayScores | None, path: Path):
+def generate_day_tile_image(day: str, year: str, languages: List[str], day_scores: DayScores | None, path: Path):
     """Saves a graphic for a given day and year. Returns the path to it."""
     image = get_alternating_background(languages, not (day_scores is None or day_scores.time2 is None))
     drawer = ImageDraw(image)
@@ -371,7 +290,7 @@ def generate_day_tile_image(day: str, year: str, languages: list[str], day_score
     image.save(path)
 
 
-def handle_day(day: int, year: int, solutions: list[str], html: HTML, day_scores: DayScores | None, needs_update: bool):
+def handle_day(day: int, year: int, solutions: List[str], html: HTML, day_scores: DayScores | None, needs_update: bool):
     languages = []
     for solution in solutions:
         extension = "." + solution.split(".")[-1]
@@ -394,7 +313,7 @@ def find_first_number(string: str) -> int:
     return int(re.findall(r"\d+", string)[0])
 
 
-def fill_empty_days_in_dict(day_to_solutions: dict[int, list[str]], max_day) -> None:
+def fill_empty_days_in_dict(day_to_solutions: Dict[int, List[str]], max_day) -> None:
     if not CREATE_ALL_DAYS and len(day_to_solutions) == 0:
         print(f"Current year has no solutions!")
     for day in range(1, max_day + 1):
@@ -402,7 +321,7 @@ def fill_empty_days_in_dict(day_to_solutions: dict[int, list[str]], max_day) -> 
             day_to_solutions[day] = []
 
 
-def handle_year(year: int, day_to_solutions: dict[int, list[str]]):
+def handle_year(year: int, day_to_solutions: Dict[int, List[str]]):
     leaderboard = request_leaderboard(year)
     if DEBUG:
         leaderboard[25] = None
@@ -442,6 +361,7 @@ def handle_year(year: int, day_to_solutions: dict[int, list[str]]):
 
 
 def main():
+    print("Running aoc-tiles")
     for year, day_to_solutions_list in get_solution_paths_dict_for_years().items():
         print(f"=== Generating table for year {year} ===")
         handle_year(year, day_to_solutions_list)
